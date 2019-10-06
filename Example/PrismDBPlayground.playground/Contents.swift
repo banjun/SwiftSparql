1bcdea7a36258b73624e26638498036df8f88d4f
banjun <banjun@gmail.com>
2019-10-06 16:38:13 +0900

    experimentally add query builder such as where and typed subjects as function builders

--- Example/PrismDBPlayground.playground/Contents.swift
+++ Example/PrismDBPlayground.playground/Contents.swift
@@ -3,6 +3,84 @@ import SwiftSparql
 import BrightFutures
 import PlaygroundSupport
 
+
+@_functionBuilder
+struct VerbObjectBuilder {
+    static func buildBlock<T>(_ builders: TripleBuilder<TripleBuilderStateRDFTypeBound<T>>...) -> [GroupGraphPatternSubType] {
+        builders.flatMap {$0.triples}
+    }
+}
+
+struct Subject<Class: RDFTypeConvertible>: TriplesBuildable {
+    var v: Var
+    var triples: [GroupGraphPatternSubType]
+}
+// should be generated
+extension Subject where Class == PrismLive {
+    init(_ varname: String, @VerbObjectBuilder builder: (TripleBuilder<TripleBuilderStateRDFTypeBound<Class>>) -> [GroupGraphPatternSubType] = {_ in []}) {
+        self.v = Var(varname)
+        self.triples = builder(subject(v).rdfTypeIsPrismLive())
+    }
+}
+// should be generated
+extension Subject where Class == PrismSong {
+    init(_ varname: String, @VerbObjectBuilder builder: (TripleBuilder<TripleBuilderStateRDFTypeBound<Class>>) -> [GroupGraphPatternSubType] = {_ in []}) {
+        self.v = Var(varname)
+        self.triples = builder(subject(v).rdfTypeIsPrismSong())
+    }
+}
+// should be generated
+extension Subject where Class == PrismEpisode {
+    init(_ varname: String, @VerbObjectBuilder builder: (TripleBuilder<TripleBuilderStateRDFTypeBound<Class>>) -> [GroupGraphPatternSubType] = {_ in []}) {
+        self.v = Var(varname)
+        self.triples = builder(subject(v).rdfTypeIsPrismEpisode())
+    }
+}
+
+protocol TriplesBuildable {
+    var triples: [GroupGraphPatternSubType] { get }
+}
+
+@_functionBuilder
+struct WhereBuilder {
+    static func buildBlock(_ builder: TriplesBuildable) -> [GroupGraphPatternSubType] {
+        builder.triples
+    }
+    static func buildBlock(_ builders: TriplesBuildable...) -> [GroupGraphPatternSubType] {
+        builders.flatMap {$0.triples}
+    }
+}
+
+struct Where {
+    var triples: [GroupGraphPatternSubType]
+
+    init(@WhereBuilder builder: () -> [GroupGraphPatternSubType]) {
+        self.triples = builder()
+    }
+}
+extension Where {
+    var query: SelectQuery {
+        SelectQuery(where: WhereClause(patterns: triples))
+    }
+}
+
+let q = Where {
+    Subject<PrismLive>("live") {
+        $0.prismPerformer(is: "solami_smile")
+        $0.prismSongPerformed(is: Var("song"))
+        $0.prismLiveOfEpisode(is: Var("ep"))
+    }
+    Subject<PrismSong>("song") {
+        $0.prismName(is: Var("title"))
+        $0.prismName(is: Var("title")) // builder args should be list
+    }
+    Subject<PrismEpisode>("ep") {
+        $0.prism話数(is: Var("n"))
+        $0.prismサブタイトル(is: Var("st"))
+    }
+}.query
+print(Serializer.serialize(q))
+
 struct EpisodeSong: Codable {
     var n: String
     var st: String
