92b655feb03481af2b8105ef7b819d9614947c6a
banjun <banjun@gmail.com>
2024-04-29 00:54:50 +0900

    remove again BrightFutures and update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftSparql",
+    platforms: [.iOS(.v15),.macOS(.v12),.watchOS(.v8),.visionOS(.v1)],
     products: [
         .library(name: "SwiftSparql", targets: ["SwiftSparql"]),
     ],
@@ -12,14 +13,13 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/kareman/FootlessParser", .upToNextMajor(from:"0.5.2")),
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift", .upToNextMajor(from:"6.0.0")),
-        .package(url: "https://github.com/Thomvis/BrightFutures", .upToNextMajor(from:"8.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftSparql",
-            dependencies: ["FootlessParser", "BrightFutures"],
+            dependencies: ["FootlessParser"],
             path: "SwiftSparql"),
         .target(
             name: "verbgen",
